eee2cc191f411554e77cbf62b746429cdbeadac9
An Le <binhanle@BERKELEY.EDU>
2018-05-10 08:17:18 -0700

    Add sound effects (part 1)

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -7,6 +7,11 @@ public class Audio : MonoBehaviour
 {
     private AudioSource audioSource;
 
+    public static AudioSource happySound;
+    public static AudioSource sadSound;
+    public static AudioSource movingSound;
+    public static AudioSource victorySound;
+
 	public void SetEnabled(Toggle toggle)
     {
         // enables or mutes the audio
@@ -17,6 +22,39 @@ public class Audio : MonoBehaviour
     {
         // set audio volume
         audioSource.volume = slider.value;
+    }
+
+    public void PlayHappy()
+    {
+        // plays happy sound
+        happySound.Play();
+    }
+
+    public void PlaySad()
+    {
+        // plays sad sound
+        sadSound.Play();
+    }
+
+    public void PlayMoving()
+    {
+        // plays moving sound
+        movingSound.Play();
+    }
+
+    public void PlayVictory()
+    {
+        // plays victory sound
+        victorySound.Play();
+    }
+
+    void Awake()
+    {
+        // initialize audio objects
+        happySound = GameObject.Find("Happy").GetComponent<AudioSource>();
+        sadSound = GameObject.Find("Sad").GetComponent<AudioSource>();
+        movingSound = GameObject.Find("Moving").GetComponent<AudioSource>();
+        happySound = GameObject.Find("Victory").GetComponent<AudioSource>();
     }
 
     void Start()
